aa80f3a476f283c8e4372c4c31c73560663705eb
Manasi Patil <134922503+ManasiP07@users.noreply.github.com>
2025-01-08 16:42:44 +0530

    added thanks api

--- webApp/server.js
+++ webApp/server.js
@@ -10,6 +10,17 @@ app.get("/api/hello", function(req, res) {
     };
     res.send(product);
 });
+app.get("/api/thanks", function(req, res) {
+    var thankyou= {
+        id: 12,
+        name: "wheel",
+        price: 233
+    };
+    res.send(thankyou);
+});
+
+
+
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
